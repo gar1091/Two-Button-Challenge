644f9976c87749131d5bafc20fc341bf0e41d7d0
Gar1091 <gar1091@icloud.com>
2024-12-20 19:48:24 -0900

    Initial Commit

---
+++ Two-Button ChallengeTests/Two_Button_ChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Two_Button_ChallengeTests.swift
+//  Two-Button ChallengeTests
+//
+//  Created by Garrett Neher on 12/20/24.
+//
+
+import Testing
+@testable import Two_Button_Challenge
+
+struct Two_Button_ChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Two-Button ChallengeUITests/Two_Button_ChallengeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Two_Button_ChallengeUITestsLaunchTests.swift
+//  Two-Button ChallengeUITests
+//
+//  Created by Garrett Neher on 12/20/24.
+//
+
+import XCTest
+
+final class Two_Button_ChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
